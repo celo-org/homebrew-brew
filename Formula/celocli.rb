1e6b11f369bd93f9c6617f5347e12cf71a94f8be
celo-org <celo-org@github.com>
2025-03-27 15:10:40 +0000

    celocli 6.2.1

--- Formula/celocli.rb
+++ Formula/celocli.rb
@@ -1,27 +1,27 @@
 class Celocli < Formula
   desc "CLI Tool for interacting with the Celo protocol"
   homepage "https://docs.celo.org/cli"
-  url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.0/celocli-v6.2.0-8ed8706ba-darwin-x64.tar.xz"
-  sha256 "38c807bae133fc5bbe8bc82a993b574e5ff73a785cf30026e00823c8e5107cbe"
-  version "6.2.0"
+  url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.1/celocli-v6.2.1-a52643b35-darwin-x64.tar.xz"
+  sha256 "847ad9d33085ac39c08eca8e7a5a2e821a7385b0ab4dc8b432f7cbd7f049ec1e"
+  version "6.2.1"
   license "Apache-2.0"
   version_scheme 1
 
   on_macos do
     on_arm do
-      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.0/celocli-v6.2.0-8ed8706ba-darwin-arm64.tar.xz"
-      sha256 "dbf8432dfd53d0a037d4d33f641462f09d859e7735afee4a604767cdc6eadecb"
+      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.1/celocli-v6.2.1-a52643b35-darwin-arm64.tar.xz"
+      sha256 "6499e32076a7ecd5d2d14378cac2644f2f31d3ade38316b6e02b770bc54d5a5f"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.0/celocli-v6.2.0-8ed8706ba-linux-x64.tar.xz"
-      sha256 "611a8568717f1c7c601a09f5f146d701c04722e8a060be1a91560a56191d9bc1"
+      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.1/celocli-v6.2.1-a52643b35-linux-x64.tar.xz"
+      sha256 "8c6b158d0291f43c6915c28f5492ebffce9e2acf81df2154c973dd994ae0351a"
     end
     on_arm do
-      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.0/celocli-v6.2.0-8ed8706ba-linux-arm.tar.xz"
-      sha256 "f1c10a7c9cc44e92a3f4061810aa9686a863e846b9cf4b56c4b3cf7e3b4f9e6d"
+      url "https://github.com/celo-org/developer-tooling/releases/download/%40celo%2Fcelocli%406.2.1/celocli-v6.2.1-a52643b35-linux-arm.tar.xz"
+      sha256 "623ad97baedc775f5e9983a480e7d03f41bb1c62012511603db6fe3e52cd42ce"
     end
   end
 
